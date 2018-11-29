947a42b0bb589e2484280b4418eb290a5136a882
Jeremy Swinnen <jswinnen@me.com>
2018-11-29 19:52:32 +0100

    Eleventy Initialisation
    - Eleventy install
    - Setting up folder structure
    - Adding Eleventy config
    - Setting up collections

---
+++ .eleventy.js
@@ -0,0 +1,15 @@
+module.exports = function(eleventyConfig) {
+   eleventyConfig.addPassthroughCopy('./src/_assets/styles/global.css');
+   eleventyConfig.addPassthroughCopy('./src/_assets/scripts/site.min.js');
+   eleventyConfig.addPassthroughCopy('./src/_assets/images');
+   return {
+      dir: {
+         input: 'src',
+         output: 'www',
+         includes: '_includes',
+         //data: '_data'
+      },
+      templateFormats: ['liquid', 'md'],
+      passthroughFileCopy: true
+   };
+};
---
+++ src/_assets/scripts/site.js
@@ -0,0 +1,18 @@
+// =======================================================================
+// Site scripts
+// =======================================================================
+
+// Masthead Modal + Toggle
+var masthead = document.querySelector('.c-masthead');
+var mastheadToggle = document.querySelector('.c-masthead__toggle');
+
+var toggleClick = function toggleClick(e) {
+   e.preventDefault();
+   masthead.classList.toggle('c-masthead--open');
+   mastheadToggle.classList.toggle('c-masthead__toggle--click');
+};
+
+mastheadToggle.addEventListener('click', toggleClick, false);
+
+// Scroller
+//$('.c-section--scroller').animate({ scrollLeft: 2200 }, 70000, "linear");
