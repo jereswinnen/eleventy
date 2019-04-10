69f3be5e6362dbe2bb5bedc525408f13ed848c43
Jeremy Swinnen <jswinnen@me.com>
2019-04-10 11:11:11 +0200

    Simplifying '.c-masthead'
    - Removing 'mobile menu'
    - Removing Javascript
    - Tweaking 'grid.scss'

--- src/_assets/scripts/site.js
+++ src/_assets/scripts/site.js
@@ -2,20 +2,5 @@
 // Site scripts
 // =======================================================================
 
-// Masthead Modal + Toggle
-var body = document.querySelector('body');
-var masthead = document.querySelector('.c-masthead');
-var mastheadToggle = document.querySelector('.c-masthead__toggle');
-var mastheadContent = document.querySelector('.c-masthead__content');
-
-var toggleClick = function toggleClick(e) {
-   e.preventDefault();
-   masthead.classList.toggle('c-masthead--open');
-   mastheadToggle.classList.toggle('c-masthead__toggle--click');
-   body.classList.toggle('no-scroll');
-};
-
-mastheadToggle.addEventListener('click', toggleClick, false);
-
 // Scroller
 //$('.c-section--scroller').animate({ scrollLeft: 2200 }, 70000, "linear");
